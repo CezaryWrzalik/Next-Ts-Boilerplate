69d88301527ec07518fa11b8838d52261d241982
CezaryWrzalik <wrzalikc@gmail.com>
2022-04-09 14:42:14 +0200

    (Feat): Setup for Styled-Components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyled } from "../shared/styles/GlobalStyles.styled";
+import { Theme } from "../types/commonTypes";
+import themeVariant from "../utils/themeVariant";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <ThemeProvider theme={themeVariant[Theme.DEFAULT]}>
+      <GlobalStyled />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
